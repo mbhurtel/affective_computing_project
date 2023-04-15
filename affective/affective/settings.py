6ddb70d2c6047345aee4f68c5c99a51cdc568041
Manish <manish.bhurtel09@gmail.com>
2023-04-15 16:41:21 -0400

    integrated videocam and music player

--- affective/affective/settings.py
+++ affective/affective/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_ROOT =os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # Default primary key field type
--- affective/affectiveApp/views.py
+++ affective/affectiveApp/views.py
@@ -26,16 +26,19 @@ class VideoCamera(object):
     def __del__(self):
         self.video.release()
 
-    def get_frame(self):
-        image = self.frame
+    def process_frame(self, image):
         detector = dlib.get_frontal_face_detector()
         faces = detector(image)
 
         for face in faces:
             # Getting the x1,y1 and x2,y2 coordinates of the face detected
             x1, y1, x2, y2 = face.left(), face.top(), face.right(), face.bottom()
+            image = cv2.rectangle(image, (x1, y1), (x2, y2), (255, 0, 0), 1)
+        return image
 
-            image = cv2.rectangle(image, (x1, y1), (x2, y2), (255,0,0), 1)
+    def get_frame(self):
+        image = self.frame
+        image = self.process_frame(image)
 
         _, jpeg = cv2.imencode('.jpg', image)
         return jpeg.tobytes()
@@ -44,6 +47,7 @@ class VideoCamera(object):
         while True:
             (self.grabbed, self.frame) = self.video.read()
 
+
 def gen(camera):
     while True:
         frame = camera.get_frame()
@@ -51,9 +55,9 @@ def gen(camera):
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
         
 
-def index(request):
-    paginator= Paginator(Song.objects.all(),1)
+def play(request):
+    paginator = Paginator(Song.objects.all(), 1)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    context={"page_obj":page_obj}
-    return render(request,"index.html",context)
+    context = {"page_obj": page_obj}
+    return render(request, "play.html", context)
